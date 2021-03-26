ef6de861409fd784c6b15f1828c7d8842ce522bb
GarrettBPowell <garettbpowell@gmail.com>
2021-03-26 17:03:41 -0500

    touch controls

--- Assets/Scripts/TouchMove.cs
+++ Assets/Scripts/TouchMove.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class TouchMove : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void Update()
     {
-        
-    }
+        if (Input.touchCount > 0)
+        {
+            // The screen has been touched so store the touch
+            Touch touch = Input.GetTouch(0);
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+            if (touch.phase == TouchPhase.Stationary || touch.phase == TouchPhase.Moved)
+            {
+                Vector3 touchPosition = Camera.main.ScreenToWorldPoint(new Vector3(touch.position.x, touch.position.y, 10));
+                transform.position = touchPosition;
+            }
+        }
     }
 }
+
